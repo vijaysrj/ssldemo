7f74d5325f84d64aea3ba8b15a4f3325033e68d6
vijay <vijay.srj@gmail.com>
2021-07-16 20:16:02 +0530

    first commit

---
+++ src/main/java/com/example/demo/SsldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsldemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "hello";
+	}
+}
